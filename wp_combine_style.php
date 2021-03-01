5af8fca6d0d0595be96dcac95f72161dcf7488dc
hosein parsa <74104897+hoseinparsa@users.noreply.github.com>
2021-03-01 10:41:09 +0330

    wp-combine-styles

---
+++ wp_combine_style.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://hparsa.ir
+ * @since             1.0.0
+ * @package           wp_combine_style
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Combine Styles
+ * Plugin URI:        http://MANABOOM.IR
+ * Description:       Combine Styles Admin Panel.
+ * Version:           1.1.0
+ * Author:            HOSEIN PARSA
+ * Author URI:        HPARSA.IR
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+
+namespace wp_combine_style;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Define Constants
+ */
+
+define( __NAMESPACE__ . '\NS', __NAMESPACE__ . '\\' );
+
+define( NS . 'PLUGIN_NAME', 'wp_combine_style' );
+
+define( NS . 'PLUGIN_VERSION', '1.0.0' );
+
+define( NS . 'PLUGIN_NAME_DIR', plugin_dir_path( __FILE__ ) );
+
+define( NS . 'PLUGIN_NAME_URL', plugin_dir_url( __FILE__ ) );
+
+define( NS . 'PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+
+define( NS . 'PLUGIN_TEXT_DOMAIN', 'wp_combine_style' );
+
+
+/**
+ * Autoload Classes
+ */
+
+require_once( PLUGIN_NAME_DIR . 'inc/libraries/autoloader.php' );
+
+/**
+ * Register Activation and Deactivation Hooks
+ * This action is documented in inc/core/class-activator.php
+ */
+
+register_activation_hook( __FILE__, array( NS . 'Inc\Core\Activator', 'activate' ) );
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented inc/core/class-deactivator.php
+ */
+
+register_deactivation_hook( __FILE__, array( NS . 'Inc\Core\Deactivator', 'deactivate' ) );
+
+
+/**
+ * Plugin Singleton Container
+ *
+ * Maintains a single copy of the plugin app object
+ *
+ * @since    1.0.0
+ */
+class wp_combine_style {
+
+	/**
+	 * The instance of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @var      Init $init Instance of the plugin.
+	 */
+	private static $init;
+	/**
+	 * Loads the plugin
+	 *
+	 * @access    public
+	 */
+	public static function init() {
+
+		if ( null === self::$init ) {
+			self::$init = new Inc\Core\Init();
+			self::$init->run();
+		}
+
+		return self::$init;
+	}
+
+}
+
+/**
+ * Begins execution of the plugin
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * Also returns copy of the app object so 3rd party developers
+ * can interact with the plugin's hooks contained within.
+ **/
+function wp_combine_style_init() {
+		return wp_combine_style::init();
+}
+
+$min_php = '5.6.0';
+
+// Check the minimum required PHP version and run the plugin.
+if ( version_compare( PHP_VERSION, $min_php, '>=' ) ) {
+		wp_combine_style_init();
+}
